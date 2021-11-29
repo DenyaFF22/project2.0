d6cd2fc1ab95c2f37d3aa1169954ec87057eb3f6
DenyaFF22 <46575152+DenyaFF22@users.noreply.github.com>
2021-11-29 19:22:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первоt число для сложения");
+            string s = Console.ReadLine( );
+            int a = Convert.ToInt32(s); 
+            
+            Console.WriteLine("Введите второе число сложения");
+            string s2 = Console.ReadLine();
+            int b = Convert.ToInt32(s2); 
+
+            int c = a + b;
+            Console.WriteLine("Сумма данных чисел:");
+            Console.WriteLine(c);
+            Console.ReadLine();
+            
+        
+        }
+    }
+}
