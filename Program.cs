28e84ae54e0ef5e44d1d297fcccf8eaf18850bd1
DenyaFF22 <den.fed.2000@mail.ru>
2021-12-05 00:07:56 +0300

    Изменил

--- Program.cs
+++ Program.cs
@@ -4,125 +4,56 @@ namespace project
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            char again = 'д';
+            while(again== 'д')
+{
+            double a;
+            double b;
+            double total;
+            char oper;
+
             Console.WriteLine("Введите первое число");
-            var s = Console.ReadLine();
-            int a = Convert.ToInt32(s); 
+            a = Convert.ToDouble(Console.ReadLine());
             
             Console.WriteLine("Введите второе число");
-            var e  = Console.ReadLine();
-            int b = Convert.ToInt32(e); 
+            b = Convert.ToDouble(Console.ReadLine());
             
               
             Console.WriteLine("Какая арефметическая операция будет выполнена");
-        
-            Console.ReadLine();
-
-            string name = "+" ;
-
-            switch (name)
-            {
-               case "+":
-                Console.WriteLine("+");
-                break;
-               case "-":
-                Console.WriteLine("-");
-                break;
-                case "*":
-                Console.WriteLine("*");
-                break;
-                case "/":
-                Console.WriteLine("/");
-                break;
-
-
-            }
-                int c = a + b;
-                Console.WriteLine("Ответ");
-                Console.WriteLine(c);
-                Console.ReadLine();
-
-                string namet = "-" ;
-
-            switch (namet)
-            {
-               case "+":
-                Console.WriteLine("+");
-                break;
-               case "-":
-                Console.WriteLine("-");
-                break;
-                case "*":
-                Console.WriteLine("*");
-                break;
-                case "/":
-                Console.WriteLine("/");
-                break;
-
-
-            }
-                int m1 = a - b;
-                Console.WriteLine("ответ");
-                Console.WriteLine(m1);
-                Console.ReadLine();  
-            string names = "*" ;
-
-            switch (names)
-            {
-               case "+":
-                Console.WriteLine("+");
-                break;
-               case "-":
-                Console.WriteLine("-");
-                break;
-                case "*":
-                Console.WriteLine("*");
-                break;
-                case "/":
-                Console.WriteLine("/");
-                break;
-
-
-            }
-                int u1 = a * b;
-                Console.WriteLine("Ответ:");
-                Console.WriteLine(u1);
-                Console.ReadLine();
-
-            var f = Console.ReadLine();
-            int m = Convert.ToInt32("-");
-            var k = Console.ReadLine();
-            int p = Convert.ToInt32("+");
-            var g = Console.ReadLine();
-            int u = Convert.ToInt32("*");
-            var h = Console.ReadLine();
-            int d = Convert.ToInt32("/");
-
-            
-            
-            
-           string namer = "/" ;
-
-            switch (namer)
-            {
-               case "+":
-                Console.WriteLine("+");
-                break;
-               case "-":
-                Console.WriteLine("-");
-                break;
-                case "*":
-                Console.WriteLine("*");
-                break;
-                case "/":
-                Console.WriteLine("/");
-                break;
-
-            }
-                int d1 = a / b;
-                Console.WriteLine("Ответ:");
-                Console.WriteLine(d1);
-                Console.ReadLine();
+            oper = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine();
+            if (oper =='+')
+                           {
+                               total = a + b;
+                               Console.WriteLine("Сумма " + total +"" );
+                            }
+                            
+                                else if (oper == '-')
+                                {
+                                    total = a - b;
+                                    Console.WriteLine("Разность " + total +"");
+                                     }
+                                     else if (oper == '*')
+                                     {
+                                         total = a * b;
+                                         Console.WriteLine("Умножение " + total +"");
+                                     }
+                               else if (oper =='/')
+                               {
+                                   total = a / b;
+                                   Console.WriteLine("Деление " + total +"");
+                               }
+                               else 
+                               {
+                                   Console.WriteLine("Неизвестный оператор");
+                               }
+                               Console.WriteLine("Хотите продолжить работу? д/н");
+                               again = Convert.ToChar(Console.ReadLine());
+                               
+                               
+                                    
+           }                    
         }
     }
 }
