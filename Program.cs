6803eb4a906ae834cc7099ae4182a8a58f52c61d
DenyaFF22 <den.fed.2000@mail.ru>
2021-12-04 23:14:24 +0300

    переделал

--- Program.cs
+++ Program.cs
@@ -1,25 +1,128 @@
-﻿using System;
+using System;
 namespace project
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Введите первое число для сложения");
+        { 
+            Console.WriteLine("Введите первое число");
             var s = Console.ReadLine();
-            int.TryParse(s, out var a);
-
-            Console.WriteLine("Введите второе число сложения");
-            s = Console.ReadLine();
-            int b = Convert.ToInt32(s);
-
-            int c = a + b;
-            Console.WriteLine("Сумма данных чисел:");
-            Console.WriteLine(c);
+            int a = Convert.ToInt32(s); 
+            
+            Console.WriteLine("Введите второе число");
+            var e  = Console.ReadLine();
+            int b = Convert.ToInt32(e); 
+            
+              
+            Console.WriteLine("Какая арефметическая операция будет выполнена");
+        
             Console.ReadLine();
 
+            string name = "+" ;
+
+            switch (name)
+            {
+               case "+":
+                Console.WriteLine("+");
+                break;
+               case "-":
+                Console.WriteLine("-");
+                break;
+                case "*":
+                Console.WriteLine("*");
+                break;
+                case "/":
+                Console.WriteLine("/");
+                break;
 
 
+            }
+                int c = a + b;
+                Console.WriteLine("Ответ");
+                Console.WriteLine(c);
+                Console.ReadLine();
+
+                string namet = "-" ;
+
+            switch (namet)
+            {
+               case "+":
+                Console.WriteLine("+");
+                break;
+               case "-":
+                Console.WriteLine("-");
+                break;
+                case "*":
+                Console.WriteLine("*");
+                break;
+                case "/":
+                Console.WriteLine("/");
+                break;
+
+
+            }
+                int m1 = a - b;
+                Console.WriteLine("ответ");
+                Console.WriteLine(m1);
+                Console.ReadLine();  
+            string names = "*" ;
+
+            switch (names)
+            {
+               case "+":
+                Console.WriteLine("+");
+                break;
+               case "-":
+                Console.WriteLine("-");
+                break;
+                case "*":
+                Console.WriteLine("*");
+                break;
+                case "/":
+                Console.WriteLine("/");
+                break;
+
+
+            }
+                int u1 = a * b;
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(u1);
+                Console.ReadLine();
+
+            var f = Console.ReadLine();
+            int m = Convert.ToInt32("-");
+            var k = Console.ReadLine();
+            int p = Convert.ToInt32("+");
+            var g = Console.ReadLine();
+            int u = Convert.ToInt32("*");
+            var h = Console.ReadLine();
+            int d = Convert.ToInt32("/");
+
+            
+            
+            
+           string namer = "/" ;
+
+            switch (namer)
+            {
+               case "+":
+                Console.WriteLine("+");
+                break;
+               case "-":
+                Console.WriteLine("-");
+                break;
+                case "*":
+                Console.WriteLine("*");
+                break;
+                case "/":
+                Console.WriteLine("/");
+                break;
+
+            }
+                int d1 = a / b;
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(d1);
+                Console.ReadLine();
         }
     }
 }
