84841acdae4cf2a65ca6fe98e6a14c3be06358d2
DenyaFF22 <den.fed.2000@mail.ru>
2021-12-05 12:15:49 +0300

    изменен

--- Program.cs
+++ Program.cs
@@ -6,54 +6,54 @@ namespace project
         static void Main(string[] args)
         {
             char again = 'д';
-            while(again== 'д')
-{
-            double a;
-            double b;
-            double total;
-            char oper;
+            while (again == 'д')
+            {
+                double a;
+                double b;
+                double total;
+                char oper;
 
-            Console.WriteLine("Введите первое число");
-            a = Convert.ToDouble(Console.ReadLine());
-            
-            Console.WriteLine("Введите второе число");
-            b = Convert.ToDouble(Console.ReadLine());
-            
-              
-            Console.WriteLine("Какая арефметическая операция будет выполнена");
-            oper = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine();
-            if (oper =='+')
-                           {
-                               total = a + b;
-                               Console.WriteLine("Сумма " + total +"" );
-                            }
-                            
-                                else if (oper == '-')
-                                {
-                                    total = a - b;
-                                    Console.WriteLine("Разность " + total +"");
+                Console.WriteLine("Введите первое число");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число");
+                b = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("Какая арефметическая операция будет выполнена");
+                oper = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine("Сумма " + total + ");
+                                }
+
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine("Разность " + total + ");
                                      }
-                                     else if (oper == '*')
-                                     {
-                                         total = a * b;
-                                         Console.WriteLine("Умножение " + total +"");
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine("Умножение " + total + ");
                                      }
-                               else if (oper =='/')
-                               {
-                                   total = a / b;
-                                   Console.WriteLine("Деление " + total +"");
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine("Деление " + total + ");
                                }
-                               else 
-                               {
-                                   Console.WriteLine("Неизвестный оператор");
-                               }
-                               Console.WriteLine("Хотите продолжить работу? д/н");
-                               again = Convert.ToChar(Console.ReadLine());
-                               
-                               
-                                    
-           }                    
+                else
+                {
+                    Console.WriteLine("Неизвестный оператор");
+                }
+                Console.WriteLine("Хотите продолжить работу? д/н");
+                again = Convert.ToChar(Console.ReadLine());
+
+
+
+            }
         }
     }
 }
